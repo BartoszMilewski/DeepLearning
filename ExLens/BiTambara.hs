1df4e3c02113878411e6c06d36ccafb10aa139aa
Bartosz Milewski <bartosz@relisoft.com>
2023-12-05 19:39:47 +0100

    Update BiTambara.hs

--- ExLens/BiTambara.hs
+++ ExLens/BiTambara.hs
@@ -1,7 +1,20 @@
 {-# language ScopedTypeVariables #-}
 module BiTambara where
 import Data.Bifunctor ( Bifunctor(second, first, bimap) )
-import Data.Binary.Builder (putInt32be)
+
+lunit_1 :: q -> ((), q)
+lunit_1 q = ((), q)
+lunit  :: ((), q) -> q
+lunit ((), q) = q
+runit_1 :: q -> (q, ())
+runit_1 q = (q, ())
+runit  :: (q, ()) -> q
+runit (q, ()) = q
+
+assoc :: ((a, b), c) -> (a, (b, c))
+assoc ((a, b), c) = (a, (b, c))
+assoc_1 :: (a, (b, c)) -> ((a, b), c)
+assoc_1 (a, (b, c))= ((a, b), c)
 
 data ExLens a da q q' s ds = 
   forall m . ExLens ((q, s)  -> (m, a))  
@@ -71,12 +84,7 @@ toTamb :: ExLens a da q q' s ds -> BiLens q q' s ds a da
 -- p (r, q) (r', q') s ds
 toTamb (ExLens fw bw) = beta . dimap fw bw . alpha
 
-lunit_1 :: q -> ((), q)
-lunit_1 q = ((), q)
-lunit  :: ((), q) -> q
-lunit ((), q) = q
-
-
+-- Composition
 
 comp :: BiLens q q' s ds a da -> BiLens r r' v dv s ds -> BiLens (q, r) (q', r') v dv a da
 -- p z z' a da -> p (z, q) (z', q') s ds
@@ -85,12 +93,72 @@ comp :: BiLens q q' s ds a da -> BiLens r r' v dv s ds -> BiLens (q, r) (q', r')
 -- p z z' a da -> p (z, (q, r)) (z', (q', r') v dv
 comp p1 p2 = dimap' assoc_1 assoc . p2 . p1
 
--- Testing
+-- Product
 
-assoc :: ((a, b), c) -> (a, (b, c))
-assoc ((a, b), c) = (a, (b, c))
-assoc_1 :: (a, (b, c)) -> ((a, b), c)
-assoc_1 (a, (b, c))= ((a, b), c)
+-- Show that a BiTambara of products is a BiTambara on both sides of the product
+
+data PRight p q r a b q' r' a' b' = PRight (p (q, q') (r, r') (a, a') (b, b'))
+
+instance (BiProfunctor p) => BiProfunctor (PRight p q r a b) where
+    dimap f g (PRight p) = PRight $ dimap (second f) (second g) p 
+    dimap' f g (PRight p) = PRight $ dimap' (second f) (second g) p
+--     dimap' :: (r -> q) -> (q' -> r') -> p q q' a b -> p r r' a b
+
+instance (BiTambara p) => BiTambara (PRight p q r a b) where
+    -- p (q, q') (r, r') (a, a') (b, b') -alpha->
+    -- p (q, q') (r, r') (m, (a, a')) (m, (b, b')) -dimap->
+    -- p (q, q') (r, r') (a, (m, a')) (b, (m, b'))
+    alpha (PRight p) = PRight $ dimap (\(a, (m, a')) -> (m, (a, a')))
+                                      (\(m, (b, b')) -> (b, (m, b'))) $ alpha p  
+    -- p (q, q')  (r, r')  (a, (q1, a')) (b, (q1', b')) - dimap->
+    -- p (q, q')  (r, r')  (q1, (a, a')) (q1', (b, b')) - beta ->
+    -- p ((q, q'), q1) ((r, r'), q1')  (a, a') (b, b') - dimap' ->
+    -- p (q, (q', q1)) (r, (r', q1'))   (a, a') (b, b')
+    beta (PRight p) = PRight $ 
+      dimap' (\(q, (q', q1)) -> ((q, q'), q1))  (\((r, r'), q1') -> (r, (r', q1'))) $
+      beta $
+      dimap (\(q1, (a, a')) -> (a, (q1, a'))) (\(b, (q1', b')) -> (q1', (b, b'))) p
+
+data PLeft p q' r' a' b' q r a b = PLeft (p (q, q') (r, r') (a, a') (b, b'))
+
+instance (BiProfunctor p) => BiProfunctor (PLeft p q r a b) where
+    dimap f g (PLeft p) = PLeft $ dimap (first f) (first g) p 
+    dimap' f g (PLeft p) = PLeft $ dimap' (first f) (first g) p
+
+instance (BiTambara p) => BiTambara (PLeft p q r a b) where
+    alpha (PLeft p) = PLeft $ dimap (\((m, a), a') -> (m, (a, a')))
+                                    (\(m, (b, b')) -> ((m, b), b')) $ alpha p  
+    beta (PLeft p) = PLeft $ 
+      dimap' (\((q, q1), q') -> ((q, q'), q1)) (\((r, r'), q1') -> ((r, q1'), r')) $
+      beta $
+      dimap  (\(q1, (a, a')) -> ((q1, a), a')) (\((q1', b), b') -> (q1', (b, b'))) p
+
+
+prodLens :: BiLens q  dq  s  ds  a  da -> 
+            BiLens q' dq' s' ds' a' da' ->
+            BiLens (q, q') (dq, dq') (s, s') (ds, ds') (a, a') (da, da')
+    -- l1 :: p1 r1 r1' a  da  -> p1 (r1, q)  (r1', dq)  s  ds
+    -- l2 :: p2 r2 r2' a' da' -> p2 (r2, q') (r2', dq') s' ds'
+    -- l3 :: p r r' (a, a') (da, da') -> p (r, (q, q')) (r', (dq, dq')) (s, s') (ds, ds')
+prodLens l1 l2 p0 = 
+    let p1 = dimap' runit runit_1 p0 
+        pl1 = PLeft p1
+        (PLeft pl2) = l1 pl1 
+        pr1 = PRight pl2 
+        (PRight pr2) = l2 pr1 
+        pr3 = dimap' (second lunit_1) (second lunit) pr2
+    in dimap' assoc_1 assoc pr3
+
+-- p0 :: (r, ()) (r', ()) (a, a') (da, da') ~ PLeft () () a' da' r r' a da
+-- l1 :: PLeft () () a' da' (r, q) (r', dq) s ds
+--       PRight (r, q) (r', dq) s ds  () () a' da'
+-- l2 :: PRigth (r, q) (r', dq) s ds ((), q') ((), dq') s' ds'
+--       ((r, q), q') ((r', dq), dq') (s, s') (ds, ds')
+-- data PRight q  r  a  b  q' r' a' b' =  (q, q') (r, r') (a, a') (b, b')
+-- data PLeft  q' r' a' b' q  r  a  b  =  (q, q') (r, r') (a, a') (b, b')
+
+
+-- Testing
 
 type D = Double
 -- Ideally, a counted vector
